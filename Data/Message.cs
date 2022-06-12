de1a970bad122e565d587684ef36e31408ce2b8b
GTEgorss <noartu@gmail.com>
2022-06-12 17:26:45 +0300

    feat: initial commit feat: home page Index.razor feat: chat page Chat.razor

---
+++ Data/Message.cs
@@ -0,0 +1,20 @@
+namespace BlazorApp.Data
+{
+    public class Message
+    {
+        public Message(string username, string body, bool mine)
+        {
+            Username = username;
+            Body = body;
+            Mine = mine;
+        }
+
+        public string Username { get; set; }
+        public string Body { get; set; }
+        public bool Mine { get; set; }
+
+        public bool IsNotice => Body.StartsWith("[Notice]");
+
+        public string CSS => Mine ? "sent" : "received";
+    }
+}
